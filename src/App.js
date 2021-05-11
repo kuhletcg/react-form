f18a3d79206675c317d3debcb43e7c5a3a848098
TCG <ssibonokuhle@gmail.com>
2021-05-11 10:50:35 +0200

    Import WorkForm and workOn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import WorkForm from "./components/WorkForm";
+import WorkOn from "./components/WorkOn";
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <WorkForm />
+      <WorkOn />
     </div>
   );
 }
